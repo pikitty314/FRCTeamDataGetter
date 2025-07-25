50eb3c633af1b6ece674d8793670cdb3206c5048
pikitty314 <pikitty314@users.noreply.github.com>
2025-07-24 22:58:18 -0400

    Picklist mode and local statbotics fallback

--- script.py
+++ script.py
@@ -7,29 +7,58 @@ import pyperclip
 YEAR = 2025
 EVENT_CODE = "2025micmp4"
 MATCH_CODE = "qm3"
+PICKLIST_MODE = False
 
 ### PULL DATA FROM STATBOTICS ###
 sb = statbotics.Statbotics() # Get Statbotics
 
-matchdata = sb.get_match(EVENT_CODE + "_" + MATCH_CODE) # Get match data
+allTeams = [None] * 6
+if (PICKLIST_MODE):
+    # NOTE: 7211B excluded as B teams are not supported
+    allTeams = ["1076", "27", "33", "51", "201", "226", "302", "1038", "1506", "1596", "2834", "3175", "3357", "3538", "3641", "3655", "3656", "4327", "4381", "4384", "4422", "4776", "5205", "5460", "5641", "5704", "6078", "6517", "7197", "7211", "8424", "8728", "9312"]
+else:
+    try:
+        matchdata = sb.get_match(EVENT_CODE + "_" + MATCH_CODE) # Get match data
 
-redAlliance = matchdata["alliances"]["red"]["team_keys"]
-blueAlliance = matchdata["alliances"]["blue"]["team_keys"]
-allTeams = redAlliance + blueAlliance
+        redAlliance = matchdata["alliances"]["red"]["team_keys"]
+        blueAlliance = matchdata["alliances"]["blue"]["team_keys"]
+        allTeams = redAlliance + blueAlliance
+    except:
+        print("It appears that Statbotics is down. Using local Statbotics data instead.\nYou'll need to manually enter the team numbers for the next match (B teams not supported).")
+        for x in range(1, 7):
+            if (x<=3):
+                allTeams[x - 1] = input("Enter team number for Red Alliance team #" + str(x) + " ")
+            else:
+                allTeams[x - 1] = input("Enter team number for Blue Alliance team #" + str(x - 3) + " ")
 
 teamData = dict()
 
+sbDataLocal = pandas.read_csv("statboticsDataLocal.csv") # Just in case
+
 for team in allTeams:
-    sbdata = sb.get_team_year(int(team), YEAR) # Get team data
+    try:
+        sbdata = sb.get_team_year(int(team), YEAR) # Get team data
 
-    teamData[str(team)] = {} # Initialize team in dict
+        teamData[str(team)] = {} # Initialize team in dict
 
-    teamData[str(team)]["total_epa"] = float(sbdata["epa"]["breakdown"]["total_points"]) # Total EPA
-    teamData[str(team)]["auto_epa"] = float(sbdata["epa"]["breakdown"]["auto_points"]) # Auto EPA
-    teamData[str(team)]["teleop_epa"] = float(sbdata["epa"]["breakdown"]["teleop_points"]) # Teleop EPA
-    teamData[str(team)]["endgame_epa"] = float(sbdata["epa"]["breakdown"]["endgame_points"]) # Endgame EPA
+        teamData[str(team)]["total_epa"] = float(sbdata["epa"]["breakdown"]["total_points"]) # Total EPA
+        teamData[str(team)]["auto_epa"] = float(sbdata["epa"]["breakdown"]["auto_points"]) # Auto EPA
+        teamData[str(team)]["teleop_epa"] = float(sbdata["epa"]["breakdown"]["teleop_points"]) # Teleop EPA
+        teamData[str(team)]["endgame_epa"] = float(sbdata["epa"]["breakdown"]["endgame_points"]) # Endgame EPA
+    except:
+        teamData[str(team)] = {}
 
+        indexCounter = 0
 
+        for target in sbDataLocal["num"]:
+            if (int(target) == int(team)):
+                teamData[str(team)]["total_epa"] = float(sbDataLocal["total_epa"].loc[sbDataLocal.index[indexCounter]]) # Total EPA
+                teamData[str(team)]["auto_epa"] = float(sbDataLocal["auto_epa"].loc[sbDataLocal.index[indexCounter]]) # Auto EPA
+                teamData[str(team)]["teleop_epa"] = float(sbDataLocal["teleop_epa"].loc[sbDataLocal.index[indexCounter]]) # Teleop EPA
+                teamData[str(team)]["endgame_epa"] = float(sbDataLocal["endgame_epa"].loc[sbDataLocal.index[indexCounter]]) # Endgame EPA
+
+            indexCounter += 1
+        
 ### PULL DATA FROM PRE SCOUTING ###
 preScouting = {}
 try:
